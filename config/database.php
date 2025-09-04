b292d98adc1bb83aa5b9f6b59d2a694baa01fdbb
Alan R <alansebas2104@gmail.com>
2025-09-04 11:15:25 -0500

    database.php y sigup,php database database connection

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+//database conenection
+$host = "localhost"; //127.0.0.1
+$user =  "postgres";
+$password = "unicesmag";
+$dbname = "marketapp";
+$port = "5432";
+
+$data_connection = "
+    host=$host
+    user=$user
+    password=$password
+    dbname=$dbname
+    port=$port
+";
+
+$conn = pg_connect($data_connection); 
+if(!$conn){
+    echo "Error";
+}else{
+    echo "Connection surccessfully mor :::  ";
+}
+
+?>
