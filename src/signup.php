4004adf9d13aa9196f2fc6fbdb0323b872f686ee
Alan R <alansebas2104@gmail.com>
2025-09-11 10:51:42 -0500

    Encryp password

--- src/signup.php
+++ src/signup.php
@@ -11,9 +11,11 @@
     $e_mail    = $_POST['email'];
     $p_wd      = $_POST['passwd'];        //contraseña
 
+    $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+
   //step3. créate query to INSERT INTO
     $query = "INSERT INTO users (firstname,lastname,mobile_number,ide_number,email,password)
-        values ('$f_name','$l_name','$m_number','$id_number','$e_mail','$p_wd')";
+        values ('$f_name','$l_name','$m_number','$id_number','$e_mail','$enc_pass')";
 
   //step4. execute query ejecutar el query (f5)
     $res = pg_query($conn, $query);
