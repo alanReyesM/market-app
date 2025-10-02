63dd6c28d04ddb354482167b2e37807891126c46
Alan R <alansebas2104@gmail.com>
2025-10-02 11:27:14 -0500

    valide el icono de main.php

--- src/main.php
+++ src/main.php
@@ -1,17 +1,23 @@
 <?php
-echo "welcome to main";
+$icon_path = __DIR__ . '/icons/market_main.png';
+$icon_web_path = 'icons/market_main.png';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Marquek-home</title>
-    <link rel="icon" type="image/png" href="../src/icons/market_main.png" />
-    <title>market - homex</title>S
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Marquek-home</title>
+    <?php if (file_exists($icon_path)): ?>
+        <link rel="icon" type="image/png" href="<?= $icon_web_path ?>" />
+    <?php else: ?>
+        <link rel="icon" href="default_favicon.ico" />
+    <?php endif; ?>
 </head>
 <body>
-    
+<?php
+echo "welcome to main";
+?>
 </body>
 </html>
