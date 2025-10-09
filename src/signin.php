e2e8765a83c62034eb05f175a4d876e98e9fdf69
Alan R <alansebas2104@gmail.com>
2025-10-09 11:50:34 -0500

    config supabase and local database connection in database file.

--- src/signin.php
+++ src/signin.php
@@ -12,14 +12,14 @@
     //step 3 query to validate data
     $sql_check_user = "
     select 
-	u.email,
-	u.password
+	  u.email,
+	  u.password
     from users u where u.email = '$e_mail' and
     u.password = '$enc_pass'
     limit 1
     ";
 //step 4. execute query 
-    $res_check = pg_query($conn, $sql_check_user);
+    $res_check = pg_query($conn_supa, $sql_check_user);
     if(pg_num_rows($res_check)>0){
       // echo "user exisrs. go to main page";
       header('refresh:0;url=main.php');
--- src/signup.php
+++ src/signup.php
@@ -14,9 +14,8 @@
     $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
     // /*jamas usar */$enc_pass = md5($p_wd);
     //validar si hay datos existentes 
-    $check_email = "
-    select u.email from users u where email = '$e_mail' or ide_number = '$id_number' limit 1 ";
-   $res_check = pg_query($conn, $check_email);
+    $check_email = "select u.email from users u where email = '$e_mail' or ide_number = '$id_number' limit 1 ";
+   $res_check = pg_query($conn_supa, $check_email);
    if(pg_num_rows($res_check) > 0){
        echo "<script>alert('user already exists')</script>";
        header('refresh:0;url=signup.html');
@@ -25,7 +24,7 @@
     $query = "INSERT INTO users (firstname,lastname,mobile_number,ide_number,email,password)
          values ('$f_name','$l_name','$m_number','$id_number','$e_mail','$enc_pass')";
           //step4. execute query ejecutar el query (f5)
-          $res = pg_query($conn, $query);
+          $res = pg_query($conn_supa, $query);
           //step5. validate result
           if($res){
          //echo "User has been created successfully mor !!!";
